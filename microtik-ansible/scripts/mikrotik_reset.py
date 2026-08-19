3f4d6b7600a7ad0cb1d6c0a00d2e23f1bffcfdc3
SandRat <sandrat@X61T>
2026-08-19 13:21:30 +0300

    Using cycles for dynamic exept command

--- microtik-ansible/scripts/mikrotik_reset.py
+++ microtik-ansible/scripts/mikrotik_reset.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
 import os
 import sys
-import pexpect
 import time
+
+import pexpect
+
 # Vars send by ansible playbook: /playbooks/00_reset.yml. Vars from file: /group_vars/mikrotik_bootstrap/vars.yml
 port = sys.argv[1]
 baud = sys.argv[2]
-password = os.getenv("BOOTSRAP_PASS")
+password = os.getenv("BOOTSTRAP_PASS")
 new_ip = sys.argv[3]
 
 print(f"Connecting to {port}...")
@@ -16,31 +17,52 @@ print(f"Connecting to {port}...")
 # encoding='utf-8' - convert bytes into string for Python.
 # timeout=30 - time for waiting answer from PTY. If no answer - raise pecect.TIMEOUT exeption.
 child = pexpect.spawn(f"picocom -b {baud} {port} -q", encoding='utf-8', timeout=30)
+# Push picocom session by empty string
+child.sendline("")
 # Add PTY log to file
 child.logfile_read = sys.stdout
-# Wating for "login" ask
-child.expect(r"login: ")
+# Wating for "login" ask. Add Character class by []. All leters inside will accept.
+child.expect(r"[Ll]ogin:\s*", timeout=5)
 # Send default "admin"
 child.sendline("admin")
 # Waiting for "password" ask
-child.expect(r"Password: ")
+child.expect(r"[Pp]assword:\s*", timeout=10)
 # Send empty string. (RouterOSv7 default password for admin is empty).
 child.sendline("")
 # Cycle for skip license: if get MikroTik license asking say "no", else - skip this step.
 try:
-    child.expect(r"Do you want to see the license", timeout=5)
-    child.sendline("n")
+    license = child.expect(
+        [
+            r"Do you want to see the software license\? \[Y/n\]:",
+            r"Press F1 for help",
+            r"\[.*@.*\] > ",
+        ],
+        timeout=10,
+    )
+    # If license text will appear - say "no". Else skip step.
+    if license == 0:
+        print("License promt detected. Bypassing...")
+        child.sendline("n\r")
+        new_pw = child.expect(
+            [
+                r"Press F1 for help",
+                r"new password:",
+                r"\[.*@.*\] > ",
+            ],
+            timeout=5,
+        )
+        if new_pw == 0:
+            child.sendline("")
+        elif new_pw == 1:
+            child.sendline("\x03")
+    elif license == 1:
+        print("Help prompt detected. Bypassing...")
+        child.sendline("")
+    elif license == 2:
+        print("Already in prompt...")
 except pexpect.TIMEOUT:
-    pass
-# Cycle for skip help: if get MikroTik first help send empty string.
-try:
-    child.expect(r"Welcome to RouterOS")
-    child.sendline("")
-except pexpect.TIMEOUT:
-    pass
-# Skipping new password creating
-child.expect(r"new password: ")
-child.sendline("\x03")
+    print("No answer from hardware!")
+    sys.exit(1)
 # Waiting for [admin@MikroTik] string start
 child.expect(r"\[.*@.*\] > ")
 # Send command reset with no factory defaults
